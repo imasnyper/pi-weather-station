23567e7d1005bde53384438b05f4cc5b77280ed4
Daniel Hayes <danihaye@gmail.com>
2018-07-04 20:58:50 -0400

    adding resmapling to rotated imaged. hopefully gets rid of the aliasing

--- weather_station.py
+++ weather_station.py
@@ -272,7 +272,7 @@ def main(debug=False, camera=False):
                 image_height, image_width = image.size
                 degrees = -2.15
 
-                image_rotated = image.rotate(degrees)
+                image_rotated = image.rotate(degrees, resample=Image.BICUBIC)
                 image_rotated_cropped = util.crop_around_center(
                     image_rotated,
                     *util.largest_rotated_rect(
