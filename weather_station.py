c4e942344a4ef43a1cd1798e91dcc3ccb523310e
Daniel Hayes <danihaye@gmail.com>
2019-05-20 17:29:45 -0400

    switching script to single run script to be executed by crontab every minute instead of handling that in script.

--- weather_station.py
+++ weather_station.py
@@ -1,3 +1,4 @@
+import pickle
 import time
 import datetime
 import os.path
@@ -57,8 +58,8 @@ class Camera:
 
         self.camera.start_recording(('/home/pi/Dev/pi-weather-station/videos/'
                                      'video-{}.h264').format(
-                                         datetime.datetime.now().strftime(
-                                             '%d-%m-%y %X')))
+            datetime.datetime.now().strftime(
+                '%d-%m-%y %X')))
         self.camera.wait_recording(length)
         self.camera.stop_recording()
         self.camera.stop_preview()
@@ -94,17 +95,23 @@ def upload_reading(unposted, debug, **kwargs):
                           data=payload)
         print(r)
         logger.info(r)
-        # for payload in unposted:
-        #     r = requests.post(upload_site,
-        #         data=payload)
-        #     if r.status_code == 201:
-        #         unposted.remove(r)
+        if r.status_code == 201:
+            for payload in unposted:
+                r = requests.post(upload_site,
+                                  data=payload)
+                if r.status_code == 201:
+                    unposted.remove(r)
+
+        return r, unposted
+
     except requests.exceptions.ConnectionError:
         print("Connection to site could not be made."
               " Storing readings to upload next time")
         logger.info("Connection to site could not be made."
                     " Storing readings to upload next time")
         unposted.append(payload)
+
+        return r, unposted
 
 
 def upload_photo(picture_file):
@@ -154,9 +161,9 @@ def upload_photo(picture_file):
 def upload_photos(picture_files):
     for picture in picture_files:
         result = upload_photo(picture)
-        if type(result) != type(1):
+        if isinstance(result, str):  # result is the picture file path in a string
             return picture_files
-        else:
+        else:  # result is 201 from the request response code
             picture_files.remove(picture)
 
     return picture_files
@@ -171,18 +178,20 @@ def round_time(dt=None, roundTo=60):
     if dt == None:
         dt = datetime.datetime.now()
     seconds = (dt.replace(tzinfo=None) - dt.min).seconds
-    rounding = (seconds+roundTo/2) // roundTo * roundTo
-    return dt + datetime.timedelta(0, rounding-seconds, -dt.microsecond)
+    rounding = (seconds + roundTo / 2) // roundTo * roundTo
+    return dt + datetime.timedelta(0, rounding - seconds, -dt.microsecond)
 
 
 def main(debug=False, camera=False):
-    loop_wait = 60 * 1
+    with open("unposted_readings.pickle", 'rb') as unposted_readings_file:
+        unposted = pickle.load(unposted_readings_file)
+    with open("unposted_photos.pickle", "rb") as unposted_photos_file:
+        unposted_photos = pickle.load(unposted_photos_file)
+    with open("last_sun_picture.pickle" "rb") as last_sun_picture_file:
+        last_sun_picture = pickle.load(last_sun_picture_file)
 
-    PICTURE_WAIT_MINUTES = 60
-
-    unposted = []
-    unposted_photos = []
-    last_sun_picture = None
+    loop_time = datetime.datetime.now()
+    loop_time_aware = pytz.timezone('Canada/Eastern').localize(loop_time)
 
     if not debug:
         windsor = Location(
@@ -200,148 +209,147 @@ def main(debug=False, camera=False):
         t_p_sensor.sea_level_pressure = t_p_sensor.read_pressure()
         camera = Camera()
 
+        dawn_time = tobermory.dawn()
+        sunrise_time = tobermory.sunrise()
+        dusk_time = tobermory.dusk()
+        sunset_time = tobermory.sunset()
 
-    while True:
-        # video_taken = False
-        loop_time = datetime.datetime.now()
-        loop_time_aware = pytz.timezone('Canada/Eastern').localize(loop_time)
+        try:
+            humidity, dht_temp = t_h_sensor.read()
+            print("DHT Humidity: {}\nDHT Temperature: {}".format(
+                humidity, dht_temp))
+            logger.info("DHT Humidity: {}\nDHT Temperature: {}".format(
+                humidity, dht_temp))
+        except Exception as e:
+            print("Error {} has occured while reading from DHT22, ignoring this reading.".format(e))
+            logger.info(
+                "Error {} has occured while reading from DHT22, ignoring this reading.".format(e))
+            dht_temp, humidity = None, None
+        try:
+            bmp_temp = t_p_sensor.read_temperature()
+            pressure = t_p_sensor.read_pressure()
+            altitude = t_p_sensor.read_altitude()
+            print("BMP Temperature: {}\nBMP Pressure: {}\nBMP Altitude: {}".format(
+                bmp_temp, pressure, altitude
+            ))
+        except Exception as e:
+            print("Error {} has occured while reading from BMP280, ignoring this reading.".format(e))
+            logger.info(
+                "Error {} has occured while reading from BMP280, ignoring this reading.".format(e))
+            bmp_temp, pressure, altitude = None, None, None
+    else:
+        dht_temp, bmp_temp, humidity, pressure, altitude = generate_random()
 
-        if not debug:
-            dawn_time = tobermory.dawn()
-            sunrise_time = tobermory.sunrise()
-            dusk_time = tobermory.dusk()
-            sunset_time = tobermory.sunset()
-
-            try:
-                humidity, dht_temp = t_h_sensor.read()
-                print("DHT Humidity: {}\nDHT Temperature: {}".format(
-                    humidity, dht_temp))
-                logger.info("DHT Humidity: {}\nDHT Temperature: {}".format(
-                    humidity, dht_temp))
-            except Exception as e:
-                print("Error {} has occured while reading from DHT22, ignoring this reading.".format(e))
-                logger.info(
-                    "Error {} has occured while reading from DHT22, ignoring this reading.".format(e))
-                dht_temp, humidity = None, None
-            try:
-                bmp_temp = t_p_sensor.read_temperature()
-                pressure = t_p_sensor.read_pressure()
-                altitude = t_p_sensor.read_altitude()
-                print("BMP Temperature: {}\nBMP Pressure: {}\nBMP Altitude: {}".format(
-                    bmp_temp, pressure, altitude
-                ))
-            except Exception as e:
-                print("Error {} has occured while reading from BMP280, ignoring this reading.".format(e))
-                logger.info(
-                    "Error {} has occured while reading from BMP280, ignoring this reading.".format(e))
-                bmp_temp, pressure, altitude = None, None, None
+    if humidity is not None and dht_temp is not None:
+        if bmp_temp is not None and pressure is not None:
+            temp = (dht_temp + bmp_temp) / 2
         else:
-            dht_temp, bmp_temp, humidity, pressure, altitude = generate_random()
+            temp = dht_temp
+            pressure = -1
 
-        if humidity is not None and dht_temp is not None:
-            if bmp_temp is not None and pressure is not None:
-                temp = (dht_temp + bmp_temp) / 2
-            else:
-                temp = dht_temp
-                pressure = -1
+        print(('{0:%d-%m-%y %X} - '
+               'Temperature = {1:0.1f}*\t'
+               'Humidity = {2:0.1f}%\t'
+               'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
+        logger.info(('{0:%d-%m-%y %X} - '
+                     'Temperature = {1:0.1f}*\t'
+                     'Humidity = {2:0.1f}%\t'
+                     'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
 
-            print(('{0:%d-%m-%y %X} - '
-                   'Temperature = {1:0.1f}*\t'
-                   'Humidity = {2:0.1f}%\t'
-                   'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
-            logger.info(('{0:%d-%m-%y %X} - '
-                         'Temperature = {1:0.1f}*\t'
-                         'Humidity = {2:0.1f}%\t'
-                         'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
+        request, unposted = upload_reading(unposted, debug, time=loop_time, temp=temp, humidity=humidity,
+                                           pressure=pressure)
 
-            upload_reading(unposted, debug, time=loop_time, temp=temp, humidity=humidity,
-                           pressure=pressure)
-
-        elif bmp_temp is not None and pressure is not None:
-            if dht_temp is not None and humidity is not None:
-                temp = (dht_temp + bmp_temp) / 2
-            else:
-                temp = bmp_temp
-                humidity = -1
-
-            print(('{0:%d-%m-%y %X} - '
-                   'Temperature = {1:0.1f}*\t'
-                   'Humidity = {2:0.1f}%\t'
-                   'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
-            logger.info(('{0:%d-%m-%y %X} - '
-                         'Temperature = {1:0.1f}*\t'
-                         'Humidity = {2:0.1f}%\t'
-                         'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
-
-            upload_reading(unposted, debug, time=loop_time, temp=temp, humidity=humidity,
-                           pressure=pressure)
-
+    elif bmp_temp is not None and pressure is not None:
+        if dht_temp is not None and humidity is not None:
+            temp = (dht_temp + bmp_temp) / 2
         else:
-            print('Failed to get reading.')
-            logger.info('Failed to get reading.')
+            temp = bmp_temp
+            humidity = -1
 
-        # stop_photo = urllib.request.urlopen("http://wasaweather.com/api/isstopped")
-        # data = stop_photo.read()
-        # data = json.loads(data)
-        # stopped = data['stopped']
+        print(('{0:%d-%m-%y %X} - '
+               'Temperature = {1:0.1f}*\t'
+               'Humidity = {2:0.1f}%\t'
+               'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
+        logger.info(('{0:%d-%m-%y %X} - '
+                     'Temperature = {1:0.1f}*\t'
+                     'Humidity = {2:0.1f}%\t'
+                     'Pressure = {3} mbar').format(loop_time, temp, humidity, pressure))
 
-        if CAMERA:
-            picture_file = None
-            # take picture every 3 minutes on the third minute, between the
-            # hours of dawn and sunrise, and dusk and sunset
-            if dawn_time <= loop_time_aware <= sunrise_time \
-                    + datetime.timedelta(minutes=20) \
-                    or sunset_time <= loop_time_aware <= dusk_time \
-                    - datetime.timedelta(minutes=20):
+        request, unposted = upload_reading(unposted, debug, time=loop_time, temp=temp, humidity=humidity,
+                                           pressure=pressure)
 
-                if not last_sun_picture or \
-                        ((loop_time_aware - last_sun_picture).seconds / 60 >= 3):
-                    picture_file = camera.take_picture(resolution=(2048, 1536))
-                    last_sun_picture = loop_time_aware
+    else:
+        print('Failed to get reading.')
+        logger.info('Failed to get reading.')
 
-            if sunrise_time <= loop_time_aware <= sunset_time:
-                if loop_time.minute == 0:
-                    picture_file = camera.take_picture(resolution=(2048, 1536))
+    # stop_photo = urllib.request.urlopen("http://wasaweather.com/api/isstopped")
+    # data = stop_photo.read()
+    # data = json.loads(data)
+    # stopped = data['stopped']
 
-            # for debugging
-            # picture_file = camera.take_picture(resolution=(2048, 1536))
+    if CAMERA:
+        picture_file = None
+        # take picture every 3 minutes on the third minute, between the
+        # hours of dawn and sunrise, and dusk and sunset
+        if dawn_time <= loop_time_aware <= sunrise_time \
+                + datetime.timedelta(minutes=20) \
+                or sunset_time <= loop_time_aware <= dusk_time \
+                - datetime.timedelta(minutes=20):
 
-            if not debug and picture_file:
-                print("Picture file created, attempting upload...")
-                logger.info("Picture file created, attempting upload...")
+            if not last_sun_picture or \
+                    ((loop_time_aware - last_sun_picture).seconds / 60 >= 3):
+                picture_file = camera.take_picture(resolution=(2048, 1536))
+                last_sun_picture = loop_time_aware
 
-                image = Image.open(picture_file)
-                image_height, image_width = image.size
-                degrees = -2.15
+        # take pictures at the top of every hour during daylight hours
+        if sunrise_time <= loop_time_aware <= sunset_time:
+            if loop_time.minute == 0:  # 0th minute of the hour AKA the top of the hour
+                picture_file = camera.take_picture(resolution=(2048, 1536))
 
-                image_rotated = image.rotate(degrees, resample=Image.BICUBIC)
-                image_rotated_cropped = util.crop_around_center(
-                    image_rotated,
-                    *util.largest_rotated_rect(
-                        image_width,
-                        image_height,
-                        math.radians(degrees)
-                    )
+        # for debugging
+        # picture_file = camera.take_picture(resolution=(2048, 1536))
+
+        if not debug and picture_file:
+            print("Picture file created, attempting upload...")
+            logger.info("Picture file created, attempting upload...")
+
+            image = Image.open(picture_file)
+            image_height, image_width = image.size
+            degrees = -2.15
+
+            image_rotated = image.rotate(degrees, resample=Image.BICUBIC)
+            image_rotated_cropped = util.crop_around_center(
+                image_rotated,
+                *util.largest_rotated_rect(
+                    image_width,
+                    image_height,
+                    math.radians(degrees)
                 )
+            )
 
-                image_rotated_cropped.save(picture_file)
+            image_rotated_cropped.save(picture_file)
 
-                result = upload_photo(picture_file)
-                # if type(result) == type(1):
-                #     if len(unposted_photos) > 0:
-                #         unposted_photos = upload_photos(unposted_photos)
-                # if type(result) != type(1):
-                #     unposted_photos.append(result)
-            else:
-                print("no picture file")
-                logger.info("no picture file")
+            result = upload_photo(picture_file)
+            if isinstance(result, int):
+                if unposted_photos:
+                    unposted_photos = upload_photos(unposted_photos)
+            elif isinstance(result, str):
+                unposted_photos.append(result)
+        else:
+            print("no picture file")
+            logger.info("no picture file")
 
-        now = datetime.datetime.now()
+    now = datetime.datetime.now()
 
-        time_taken = now - loop_time
-        print('Loop took {} seconds.'.format(time_taken.seconds))
-        print('Sleeping {} seconds...'.format(loop_wait - now.second))
-        time.sleep(loop_wait - now.second)
+    time_taken = now - loop_time
+    print('Loop took {} seconds.'.format(time_taken.seconds))
+
+    with open("unposted_readings.pickle", "wb") as unposted_readings_file:
+        pickle.dump(unposted, unposted_readings_file)
+    with open("unposted_photos.pickle", "wb") as unposted_photos_file:
+        pickle.dump(unposted_photos, unposted_photos_file)
+    with open("last_sun_photo.pickle", "wb") as last_sun_picture_file:
+        pickle.dump(last_sun_picture, last_sun_picture_file)
 
 
 if __name__ == '__main__':
